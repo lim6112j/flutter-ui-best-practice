616b2031a400608508d46b7863cff1966556f29e
lim6112j <lim6112j@gmail.com>
2022-06-03 02:01:20 +0900

    test added

---
+++ test/math_test.dart
@@ -0,0 +1,11 @@
+//test
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('math test', () {
+    print(pow(3, 2));
+    expect(pow(3, 2), 8);
+  });
+}
