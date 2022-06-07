b8a71455ee41aa2fbb8749a30fffcde7c3aaff32
lim6112j <lim6112j@gmail.com>
2022-06-07 21:39:24 +0900

    transformcontroller added for size tracking

--- lib/screens/home/components/hero_detail_view.dart
+++ lib/screens/home/components/hero_detail_view.dart
@@ -37,12 +37,51 @@ class _CallbackBuchAlgorithm extends BuchheimWalkerAlgorithm {
     positionNodes(graph);
     var graphSize = calculateGraphSize(graph);
     shiftCoordinates(graph, (mediaSize.width - graphSize.width) / 2, shiftY);
-    //shiftCoordinates(graph, (mediaSize.width - graphSize.width) / 2, shiftY);
+    //shiftCoordinates(graph, shiftX, shiftY);
     return calculateGraphSize(graph);
   }
 }
 
-class _HeroDetailViewState extends State<HeroDetailView> {
+class _HeroDetailViewState extends State<HeroDetailView>
+    with TickerProviderStateMixin {
+  final TransformationController _transformationController =
+      TransformationController();
+  Animation<Matrix4>? _animationReset;
+  late final AnimationController _controllerReset;
+
+  void _onAnimateReset() {
+    _transformationController.value = _animationReset!.value;
+    if (!_controllerReset.isAnimating) {
+      _animationReset!.removeListener(_onAnimateReset);
+      _animationReset = null;
+      _controllerReset.reset();
+    }
+  }
+
+  void _animateResetInitialize() {
+    _controllerReset.reset();
+    _animationReset = Matrix4Tween(
+      begin: _transformationController.value,
+      end: Matrix4.identity(),
+    ).animate(_controllerReset);
+    _animationReset!.addListener(_onAnimateReset);
+    _controllerReset.forward();
+  }
+
+  void _animateResetStop() {
+    _controllerReset.stop();
+    _animationReset?.removeListener(_onAnimateReset);
+    _animationReset = null;
+    _controllerReset.reset();
+  }
+
+  void _onIteractionStart(ScaleStartDetails details) {
+    print("animation starting ....");
+    if (_controllerReset.status == AnimationStatus.forward) {
+      _animateResetStop();
+    }
+  }
+
   Random r = Random();
   Widget rectangleWidget(Gecko gecko) {
     return InkWell(
@@ -142,6 +181,14 @@ class _HeroDetailViewState extends State<HeroDetailView> {
       ..levelSeparation = (20)
       ..subtreeSeparation = (10)
       ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_BOTTOM_TOP);
+    _controllerReset = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 400));
+  }
+
+  @override
+  void dispose() {
+    _controllerReset.dispose();
+    super.dispose();
   }
 
   @override
@@ -196,12 +243,17 @@ class _HeroDetailViewState extends State<HeroDetailView> {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+              ),
               Expanded(
                 child: InteractiveViewer(
+                    transformationController: _transformationController,
+                    onInteractionStart: _onIteractionStart,
                     constrained: false,
                     boundaryMargin: EdgeInsets.all(10),
                     minScale: 1.0,
-                    maxScale: 3.6,
+                    maxScale: 1.0,
                     child: GraphView(
                       graph: graph,
                       algorithm: _CallbackBuchAlgorithm(
