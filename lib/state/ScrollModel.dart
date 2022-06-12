73c34a13e1b7c1def6c9165ecdbe0c8477f955d5
lim6112j <lim6112j@gmail.com>
2022-06-12 15:50:32 +0900

    inittial hidden bug resolved

--- lib/state/ScrollModel.dart
+++ lib/state/ScrollModel.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ScrollModel extends ChangeNotifier {
-  bool _hidden = true;
+  bool _hidden = false;
   bool get hidden => _hidden;
   void changeHidden(bool hiddenValue) {
     print("scroll model changehidden called >>>>>>>");
